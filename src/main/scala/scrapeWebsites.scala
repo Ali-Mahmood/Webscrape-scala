ec625ba2cca534c11a860827d44ea0e50082c91e
amd25 <ali.mahmood@sky.uk>
2018-07-27 17:02:03 +0100

    testing

--- src/main/scala/scrapeWebsites.scala
+++ src/main/scala/scrapeWebsites.scala
@@ -1,6 +1,7 @@
 import net.ruippeixotog.scalascraper.browser.Browser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import scala.collection.breakOut
 
 object scrapeWebsites {
 
@@ -8,19 +9,28 @@ object scrapeWebsites {
     sites.foreach { site =>
       val page = browser.get(site)
 
-      val maybeHeadlines: Option[List[String]] = site match {
-
+      val maybeHeadlines = site match {
         case site if site.contains("news.sky.com") =>
-          page tryExtract elementList("h3 a") map (titles => titles.map(_.text))
+          val headers: Option[List[String]] = page tryExtract elementList("h3 a") map (titles => titles.map(_.text))
+          val hrefs: Option[List[Option[String]]] = page tryExtract elementList("h3 a") tryExtract attr("href")("a")
+
+          for {
+            head <- headers
+            hre <- hrefs
+            mapping <- Map(head -> hre)
+          }
 
         case site if site.contains("www.bbc.co.uk") =>
-          page tryExtract elementList("a h3") map (titles => titles.map(_.text))
+          val headlines = page tryExtract elementList("a h3") map (titles => titles.map(_.text))
+          val href = page tryExtract elementList("a h3") tryExtract attr("href")("a")
+          Map(headlines -> href)
 
         case _ =>
           println("Website not supported")
           None
       }
-      maybeHeadlines.map(headlines => dBHandler.saveHeadlines(headlines.distinct, site))
+      println(maybeHeadlines)
+      //maybeHeadlines.map(headlines => dBHandler.saveHeadlines(headlines.distinct, site))
       println(site)
     }
   }
